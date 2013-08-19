9f79b13022015397b9aa702a6e49e3b57062dca8
Drackenov <Drackenov@gmail.com>
2013-08-18 22:52:07 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bollocks::Application.config.secret_key_base = '4e59390511ed5638101d90c4e88744298e9e6fc7b76e3745ef7102a0359a03763ca5cee3d8d8cf9c9c55fd7b0fcb5a997ec3685a38ac3328c0dbdbf911dc2082'
